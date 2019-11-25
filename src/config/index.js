28c7193d879e86966ddff11e2423ac2294cb6f4e
Jean <jean.george.alves@gmail.com>
2019-11-25 02:26:19 -0300

    fixing problems

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   mysql: {
-    host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    host: "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "p4maq6ylr0593mjn",
     database: "ihc4db69jvl4ir4c",
