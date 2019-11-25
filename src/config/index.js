fb5bccceb3afbf651a7181e8ea455deb33679dc6
Jean <jean.george.alves@gmail.com>
2019-11-25 02:16:11 -0300

    ajeitando index.js

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const url = co;
+
+module.exports = {
+  mysql: {
+    host: process.env.MY_HOST ? process.env.MY_HOST : "localhost",
+    port: 3306,
+    user: process.env.MY_USER ? process.env.MY_USER : "root",
+    database: process.env.MY_DATABASE ? process.env.MY_DATABASE : "tp2",
+    password: process.env.MY_PASSWORD ? process.env.MY_PASSWORD : ""
+  }
+};
